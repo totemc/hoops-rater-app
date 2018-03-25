761e6f45e6004c5a6e0c0ae0f5550b2acdc897ed
Adrian Rodriguez <adrianr0120@gmail.com>
2018-03-25 16:40:26 -0400

    Restructured files, added express backend + proxy

---
+++ client/src/profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+
+class Profile extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+			<div>
+				<span style={{fontSize:"3em"}}>{this.props.fname}</span>
+				<br></br>
+				<span style={{fontSize:"2em"}}>{this.props.lname}</span>
+				<br></br>
+				<span style={{textAlign:"left"}}>{this.props.age}</span>
+				<br></br>
+				<span style={{textAlign:"left"}}>{this.props.email}</span>
+			</div>
+
+		)
+	}
+}
+
+export default Profile;
