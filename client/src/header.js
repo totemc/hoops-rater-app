3f9674f7f2fbfcb6301ea6417c2c3a0fc0f46d16
unknown <adrianr0120@gmail.com>
2018-03-30 16:30:24 -0400

    added some links that work with routing

--- client/src/header.js
+++ client/src/header.js
@@ -6,12 +6,16 @@ class Header extends React.Component{
 			return (
 				<div>
 					<h1> This is my header component. I will be styled later. You are logged in.</h1>
+					<a href="/">Main</a>
+					<a href="/profile/user1">User1 Profile</a>
 				</div>
 			)
 		}else{
 			return (
 				<div>
 					<h1> This is my header component. I will be styled later. You are not logged in.</h1>
+					<a href="/">Main</a>
+					<a href="/profile/user1">User1 Profile</a>
 				</div>
 			)
 		}
