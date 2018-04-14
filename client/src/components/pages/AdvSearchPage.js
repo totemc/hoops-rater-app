799e2039cf809a098035805227be36eac137d4b1
Oscar Padilla <padillaoscara@gmail.com>
2018-04-14 00:29:40 -0400

    Manually added AdvSearchPage.js

---
+++ client/src/components/pages/AdvSearchPage.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import {Redirect} from 'react-router-dom';
+
+class AdvSearch extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            madeSearch: false,
+            value:'default',
+            court_zip:'default',
+            outdoor_status: 'default',
+            rating: 'default',
+            open_time: 'default',
+            close_time: 'default',
+            membership_status: 'default',
+            busiest_times: 'default'
+        };
+        this.handleChangeZipcode = this.handleChangeZipcode.bind(this);
+        this.handleChangeRating = this.handleChangeRating.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeBusiestTime = this.handleChangeBusiestTime.bind(this);
+    }
+    
+    handleChangeZipcode(e){
+        console.log('OK ADRIAN!')
+        this.setState({court_zip: e.target.value})
+        //console.log(this.state.court_zip)
+    }
+    
+    handleChangeBusiestTime(e){
+        this.setState({busiest_times: e.target.value})
+    }
+    
+    handleChangeMemberStatus(e){
+        this.setState({membership_status: e.target.value})
+    }
+    
+    handleChangeOpenHours(e){
+        this.setState({open_time: e.target.value})
+    }
+    
+    handleChangeCloseHours(e){
+        this.setState({close_time: e.target.value})
+    }
+       
+    handleChangeOutdoorStatus(e){
+        this.setState({outdoor_status: e.target.value})
+    }
+      
+    handleChangeRating(e){
+        this.setState({rating: e.target.value})
+    }
+    
+    handleSubmit(e){
+        e.preventDefault();
+        let newValue = '/advsearch/court/';
+        Object.keys(this.state).map((key) => {
+            if (this.state[key] != 'default'){
+                if(newValue == '/advsearch/court/'){
+                    newValue += (key+'='+this.state[key])
+                } else{
+                    newValue += ('+' + key + '=' + this.state[key])
+                }
+            }
+        })
+        this.setState((prev) => ({
+            madeSearch: true,
+            value: newValue
+        }))
+        
+        //this.setState({value: e.target.newValue})
+        console.log(this.state.value)
+    }
+
+    render(){
+//        console.log("court" + this.state.court_zip)
+//        console.log("rating" + this.state.rating)
+        console.log('value' + this.state.value)
+        if(!this.state.madeSearch){
+            return(
+                <form onSubmit={this.handleSubmit}>
+                <input type='text' placeholder='Enter Zipcode' onChange={this.handleChangeZipcode}/>
+                <label>test
+                <br></br>
+                <select name='value' onChange={this.handleChangeBusiestTime}>
+                    <option value='default'>default</option>
+                    <option value='Excellent'>Excellent</option>
+                    <option value='Good'>Good</option>
+                    <option value='Poor'>Poor</option>
+                </select>
+                </label>
+                <label>anotherOne
+                <br />
+                <select onChange={this.handleChangeRating}>
+                    <option value='default'>default</option>
+                    <option value='Excellent'>Excellent</option>
+                    <option value='Good'>Good</option>
+                    <option value='Poor'>Poor</option>
+                </select>
+                </label>
+                <input type='submit' value='Submit' />
+                </form>
+            );
+        } else{
+            return <Redirect to={this.state.value} />
+        }
+    }
+}
+export default AdvSearch;
