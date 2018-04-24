30d9e12b69891a76711d16d7a796a7fb0ec864dc
Adrian <adrianr0120@gmail.com>
2018-04-24 00:05:31 -0400

    fixed court view page styles

--- client/src/components/pages/CourtViewPage.js
+++ client/src/components/pages/CourtViewPage.js
@@ -3,12 +3,14 @@ import {Redirect} from 'react-router-dom';
 import NotFound from './NotFoundPage';
 import StarRatingComponent from 'react-star-rating-component';
 import { Grid, Row, Col, Panel, Button, FormGroup, FormControl } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 
 const styles = {
     panelStyle : {
         background : "rgba(248, 201, 149, 1)",
         color: "darkslategrey",
-        borderColor: "rgba(222,127,24,.2)"
+        borderColor: "rgba(222,127,24,.2)",
+
     }
 }
 
@@ -95,94 +97,85 @@ class CourtView extends React.Component{
                         <Col lg={12}>
                             {this.state.response.slice(0,1).map((item) => (
                                 <div>
-                                    <Row>
-                                        <Col lg={10}>
-                                            <h4>Court Name: </h4>
-                                            <h2>{item.court_name}</h2>
-                                        </Col>
-                                        <Col lg={1}>
-                                            <h4>Rating: </h4>
-                                            <h2>{item.avg_stars}</h2>
-                                        </Col>
-                                        <Col lg={12}>
-                                            <h4>Address: </h4>
-                                            <h2>{item.address}  {item.court_zip}</h2>
-                                        </Col>
-                                    </Row>
-                                    <Panel style={styles.panelStyle}>
+                                    <section style={{margin:"20px 40px"}}>
+                                        <Row>
+                                            <Col lg={10}>
+                                                <span style={{fontSize:"24px"}}>Location</span>
+                                                <h1>{item.court_name.toUpperCase()}</h1>
+                                            </Col>
+                                            <Col lg={1} lgOffset={1} style={{textAlign:"center"}}>
+                                                <FontAwesome name="star" size="2x"/>
+                                                <h1>{item.avg_stars}</h1>
+                                            </Col>
+                                            <Col lg={12}>
+                                                <h2>{item.address}  {item.court_zip}</h2>
+                                            </Col>
+                                        </Row>
+                                    </section>
+                                    <Panel style={styles.panelStyle} >
                                         <Panel.Body>
-                                            <Row>
+                                            <Row className="boldFont">
                                                 <Col lg={2}>
                                                     <h4>busiest times</h4>
                                                     {item.busiest_times}
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>open time</h4>
+                                                    {item.open_time + ' AM'}
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>close time</h4>
+                                                    {item.close_time + ' PM'}
                                                 </Col>
                                                 <Col lg={2}>
                                                     <h4>court size</h4>
                                                     {item.court_size}
                                                 </Col>
                                                 <Col lg={2}>
+                                                    <h4>membership status</h4>
+                                                    {item.membership_status.toString()}
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>indoor status</h4>
+                                                    {item.outdoor_status.toString()}
+                                                </Col>
+                                                <Col lg={12}>
+                                                    <br/>
+                                                </Col>
+                                                <Col lg={2}>
                                                     <h4>hoop height</h4>
                                                     {item.hoop_height}
                                                 </Col>
                                                 <Col lg={2}>
-                                                    <h4>membership status</h4>
-                                                    {item.membership_status}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>Outdoor status</h4>
-                                                    {item.outdoor_status}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>hoop height</h4>
-                                                    {item.busiest_times}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>membership status</h4>
-                                                    {item.busiest_times}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>indoor status</h4>
-                                                    {item.busiest_times}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>net status</h4>
-                                                    {item.busiest_times}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>open time</h4>
-                                                    {item.open_time}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>close time</h4>
-                                                    {item.close_time}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>Has fountain</h4>
-                                                    {item.has_fountain}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>Has vending machine</h4>
-                                                    {item.has_vending_machine}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>Pavement quality</h4>
-                                                    {item.pavement_quality}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>Cleanliness</h4>
-                                                    {item.cleanliness}
-                                                </Col>
-                                                <Col lg={2}>
-                                                    <h4>Rim quality</h4>
-                                                    {item.rim_quality}
-                                                </Col>
-                                                 <Col lg={2}>
-                                                    <h4>Net quality</h4>
+                                                    <h4>net quality</h4>
                                                     {item.net_quality}
                                                 </Col>
                                                 <Col lg={2}>
-                                                    <h4>Net type</h4>
+                                                    <h4>net type</h4>
                                                     {item.net_type}
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>water fountain?</h4>
+                                                    {item.has_fountain.toString()}
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>vending machine?</h4>
+                                                    {item.has_vending_machine.toString()}
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>pavement quality</h4>
+                                                    {item.pavement_quality}
+                                                </Col>
+                                                <Col lg={12}>
+                                                    <br/>
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>cleanliness</h4>
+                                                    {item.cleanliness}
+                                                </Col>
+                                                <Col lg={2}>
+                                                    <h4>rim quality</h4>
+                                                    {item.rim_quality}
                                                 </Col>
                                             </Row>
                                         </Panel.Body>
@@ -191,24 +184,21 @@ class CourtView extends React.Component{
                             ))}
                         </Col>
                     </Row>
+                    <section style={{margin:"20px 40px"}}>
+                        <h1>Comments</h1>
+                    </section>
                     <Panel style={styles.panelStyle}>
                         <Panel.Body>
-                            <Row>
-                                <Col lg={12}>
-                                    <span>
-                                        {this.state.response.map((comment, index) => (
-                                            <Row>
-                                                <Col lg={12}>
-                                                    <h1>{comment.comment_username}</h1>
-                                                </Col>
-                                                <Col lg={12}>
-                                                    <p>{comment.comment_text}</p>
-                                                </Col>
-                                            </Row>
-                                        ))}
-                                    </span>
-                                </Col>
-                            </Row>
+                            {this.state.response.slice(1).map((comment, index) => (
+                                <Row>
+                                    <Col lg={12}>
+                                        <h1>{comment.comment_username}</h1>
+                                    </Col>
+                                    <Col lg={12} style={{margin:"0px 20px"}}>
+                                        <p>{comment.comment_text}</p>
+                                    </Col>
+                                </Row>
+                            ))}
                         </Panel.Body>
                     </Panel>
                     <Panel style={styles.panelStyle}>
