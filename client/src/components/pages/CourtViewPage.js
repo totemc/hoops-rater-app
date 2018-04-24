dbdbcbbbaa75c7b98190086acc89d41c867ea781
amirjindani <amirjindani@gmail.com>
2018-04-23 20:54:42 -0400

    courtview update

--- client/src/components/pages/CourtViewPage.js
+++ client/src/components/pages/CourtViewPage.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
 import NotFound from './NotFoundPage';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Panel, Button, FormGroup, FormControl } from 'react-bootstrap';
+
+const styles = {
+    panelStyle : {
+        background : "rgba(248, 201, 149, 1)",
+        color: "darkslategrey",
+        borderColor: "rgba(222,127,24,.2)"
+    }
+}
 
 class CourtView extends React.Component{
 	constructor(props){
@@ -46,7 +54,7 @@ class CourtView extends React.Component{
 	callApi = async () => {
 	  const response = await fetch('/api/court/'+this.props.match.params.id);
 	  const body = await response.json();
-
+      //console.log(body.slice(0,1));
 	  if (response.status !== 200) {
 	  	throw Error(body.message)
 	  };
@@ -61,133 +69,143 @@ class CourtView extends React.Component{
 		}
 		return (
 			<div>
-				<span style={{fontSize:"3em"}}>{this.props.match.params.id}</span>
-				<br></br>
                 
      <Grid>
-        <Row className="show-grid">
-            <Col xs={6} md={4}>
-                <code>Park Name:<span style={{fontSize:"3em"}}>{this.state.response.map((item, court_name) => (<p>{item.court_name}</p>))}</span></code>
+        <Row>
+            <Col lg={12}>
+                {this.state.response.slice(0,1).map((item) => (
+                    <div>
+                        <Row>
+                            <Col lg={10}>
+                                <h4>Court Name: </h4>
+                                <h2>{item.court_name}</h2>
+                            </Col>
+                            <Col lg={1}>
+                                <h4>Rating: </h4>
+                                <h2>{item.avg_stars}</h2>
+                            </Col>
+                            <Col lg={12}>
+                                <h4>Address: </h4>
+                                <h2>{item.address}  {item.court_zip}</h2>
+                            </Col>
+                        </Row>
+                        
+                        <Panel style={styles.panelStyle}>
+                            <Panel.Body>
+                                <Row>
+                                    <Col lg={2}>
+                                        <h4>busiest times</h4>
+                                        {item.busiest_times}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>court size</h4>
+                                        {item.court_size}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>hoop height</h4>
+                                        {item.hoop_height}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>membership status</h4>
+                                        {item.membership_status}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>Outdoor status</h4>
+                                        {item.outdoor_status}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>net status</h4>
+                                        {item.busiest_times}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>open time</h4>
+                                        {item.open_time}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>close time</h4>
+                                        {item.close_time}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>Has fountain</h4>
+                                        {item.has_fountain}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>Has vending machine</h4>
+                                        {item.has_vending_machine}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>Pavement quality</h4>
+                                        {item.pavement_quality}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>Cleanliness</h4>
+                                        {item.cleanliness}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>Rim quality</h4>
+                                        {item.rim_quality}
+                                    </Col>
+                                     <Col lg={2}>
+                                        <h4>Net quality</h4>
+                                        {item.net_quality}
+                                    </Col>
+                                    <Col lg={2}>
+                                        <h4>Net type</h4>
+                                        {item.net_type}
+                                    </Col>
+                                </Row>
+                            </Panel.Body>
+                        </Panel>
+                    </div>
+                ))}
             </Col>
-            
-            
-            
-            <Col xs={6} md={4}>
-                <code>Rating:<span style={{fontSize:"3em"}}>{this.state.response.map((item, avg_stars) => (<p>{item.avg_stars}</p>))}</span></code>
+           
+        </Row>
+
+	
+        <Panel style={styles.panelStyle}>
+         <Panel.Body>
+
+        <Row>
+            <Col lg={12}>
+				Comment: 
+                <span>{this.state.response.map((comment, index) => (
+                       <Row>
+                       <Col lg={12}>
+                            <h1>{comment.comment_username}</h1>
+                       </Col>
+        
+                       <Col lg={12}>
+                            <p>{comment.comment_text}</p>
+                       </Col>
+                        </Row>
+                ))}
+                </span>
             </Col>
-				
+        </Row>
+            </Panel.Body>
+             </Panel>
+
     
-            <Col xs={6} md={4}>
-				<code>Address:<span style={{fontSize:"3em"}}>{this.state.response.map((item, address) => (<p>{item.address}</p>))}</span></code>
-                <code>Zipcode: <span style={{fontSize:"3em"}}>{this.state.response.map((item, court_zip) => (<p>{item.court_zip}</p>))}</span></code> 
-            </Col>
-        </Row>
-
-				<br></br>
-                                                                                                  
-                                                                                                    
-        <Row className="show-grid">
-            <Col xs={6} md={4}>
-                <code>Busiest time: <span style={{fontSize:"3em"}}>{this.state.response.map((item, busiest_times) => (<p>{item.busiest_times}</p>))}</span></code>
-            </Col>
-        
-            <Col xs={6} md={4}>
-				<code>Outdoor status: <span style={{fontSize:"3em"}}>{this.state.response.map((item, outdoor_status) => (<p>{item.outdoor_status}</p>))}</span></code>
-            </Col>
-                                                                                                   
-            <Col xs={6} md={4}>
-				<code>Membership status: <span style={{fontSize:"3em"}}>{this.state.response.map((item, membership_status) => (<p>{item.membership_status}</p>))}</span></code>
-            </Col>
-        </Row>
-                                                                                                            
-				<br></br>
-        
-        <Row className="show-grid">
-            <Col md={6} mdPush={6}>
-				<code>Open time: <span style={{fontSize:"3em"}}>{this.state.response.map((item, open_time) => (<p>{item.open_time}</p>))}</span></code>
-            </Col>
-        
-            <Col md={6} mdPull={6}>
-				<code>Close time: <span style={{fontSize:"3em"}}>{this.state.response.map((item, close_time) => (<p>{item.close_time}</p>))}</span></code>
-            </Col>
-        </Row>
-                                                                                                         
-				<br></br>
-                                                                                                                 
-        <Row className="show-grid">
-            <Col md={6} md={6}>                                                                      
-				<code>Has Fountain: <span style={{fontSize:"3em"}}>{this.state.response.map((item, has_fountain) => (<p>{item.has_fountain}</p>))}</span></code>
-            </Col>
-				
-            <Col md={6} md={6}> 
-				<code>Has Vending machine: <span style={{fontSize:"3em"}}>{this.state.response.map((item, has_vending_machine) => (<p>{item.has_vending_machine}</p>))}</span></code>
-            </Col>
-        </Row>
-				<br></br>
-
-        <Row className="show-grid">
-            <Col xs={12} md={12}>
-				<code>Court size: <span style={{fontSize:"3em"}}>{this.state.response.map((item, court_size) => (<p>{item.court_size}</p>))}</span></code>
-            </Col>
-        </Row>
-
-				<br></br>
-
-        <Row className="show-grid">
-            <Col xs={6} md={4}>
-				<code>Pavement quality: <span style={{fontSize:"3em"}}>{this.state.response.map((item, pavement_quality) => (<p>{item.pavement_quality}</p>))}</span></code>
-			</Col>
-            
-            <Col xs={6} md={4}>
-				<code>Cleanliness: <span style={{fontSize:"3em"}}>{this.state.response.map((item, cleanliness) => (<p>{item.cleanliness}</p>))}</span></code>
-			</Col>
-
-            <Col xs={6} md={4}>
-				<code>Rim quality: <span style={{fontSize:"3em"}}>{this.state.response.map((item, rim_quality) => (<p>{item.rim_quality}</p>))}</span></code>
-            </Col>
-        </Row>
-
-				<br></br>
-
-        <Row className="show-grid">
-            <Col xs={6} md={4}>
-				<code>Net quality: <span style={{fontSize:"3em"}}>{this.state.response.map((item, net_quality) => (<p>{item.net_quality}</p>))}</span></code>
-            </Col>
-
-            <Col xs={6} md={4}>
-                <code>Net type: <span style={{fontSize:"3em"}}>{this.state.response.map((item, net_type) => (<p>{item.net_type}</p>))}</span></code>
-            </Col>
-
-            <Col xs={6} md={4}>
-				<code>Hoop height: <span style={{fontSize:"3em"}}>{this.state.response.map((item, hoop_height) => (<p>{item.hoop_height}</p>))}</span></code>
-            </Col>
-        </Row>
-
-
-				<br></br>
-
-
-        <Row className="show-grid">
-            <Col md={6} md={6}>
-				<code>Comment: <span style={{fontSize:"3em"}}>{this.state.response.map((comment, index) => (<p key={index}>{comment.comment_username} {comment.comment_text}</p>))}</span></code>
-            </Col>
-
-            <Col md={6} md={6}>
-				<code>Comment: <span style={{fontSize:"3em"}}>{this.state.response.map((visited, index) => (<p key={index}>{visited.visited_username} {visited.has_visited}</p>))}</span></code>
-            </Col>
-        </Row>
-
-    </Grid>;
-
-				<span style={{textAlign:"left"}}>{this.state.response.map((visited, index) => (
-					<p key={index}>{visited.visited_username} {visited.has_visited}</p>
-				))}</span>
-                <form onSubmit={this.handleSubmit}>
-                    Email Address:<br/>
-                    <input type="text" name="comment" onChange={this.handleChange}/><br/>
-                    <input type="submit" value="Add Comment"/>
+            <Panel style={styles.panelStyle}>
+         <Panel.Body>
+                 <Row>
+            <Col lg={12}>
+                <form  onSubmit={this.handleSubmit}>
+                     <FormGroup controlId="formBasicText">
+                        <FormControl type="text"  placeholder="comment" onChange={this.handleChange}/>
+                     </FormGroup>
+                        <Button type = "submit"> Add Comment</Button>
                 </form>
+               </Col>
+        </Row>
+     </Panel.Body>
+             </Panel>
+
+    </Grid>
 			</div>
+
+    
 		)
 	}
 }
